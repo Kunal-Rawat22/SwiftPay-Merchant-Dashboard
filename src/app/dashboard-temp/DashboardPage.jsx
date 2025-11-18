333aedb14f47aecccc323267a6d1fd7e616fb10d
Kunal Rawat <Kunalrawat399@gmail.com>
2025-11-18 23:47:52 +0530

    SPFD-1: Added Project Structure and removed unnecessary files

---
+++ src/app/dashboard-temp/DashboardPage.jsx
@@ -0,0 +1,4 @@
+export default function DashboardPage() {
+    //use multiple components
+    return <><h1>Dashboard Page</h1></>;
+}
---
+++ src/app/dashboard-temp/page.jsx
@@ -0,0 +1,8 @@
+import DashboardPage from "@/app/dashboard-temp/DashboardPage";
+
+export default function Dashboard()
+{
+
+    // API calling
+    return <DashboardPage/>;
+}
